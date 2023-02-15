b8a9e2bc1ac19f3d4c73e96d59e7a271f912a9d1
juancenteno10 <juancsabillon06@gmail.com>
2023-02-15 13:49:53 -0600

    Creacion GridView,

--- SistemaReparaciónDePC/Clases/Cliente.cs
+++ SistemaReparaciónDePC/Clases/Cliente.cs
@@ -1,11 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
+using System.Web.UI.WebControls;
 
 namespace SistemaReparaciónDePC.Clases
 {
     public class Cliente
     {
+        Utilitario util = new Utilitario();
+       public void CargarGriv(GridView gv, string buscar)
+        {
+            if(buscar == "")
+            {
+                DataSet ds = util.ObtenerDatos("UDP_MostrarClientes", "t");
+                gv.DataSource = ds.Tables["t"];
+                gv.DataBind();
+
+            }
+            else
+            {
+                string sql = "UDP_BuscarClientes'" + buscar + "'";
+                DataSet ds = util.ObtenerDatos(sql,"t");
+                gv.DataSource = ds.Tables["t"];
+                gv.DataBind();
+            }
+
+        }
+    
+    
     }
 }
--- SistemaReparaciónDePC/Cliente_Index.aspx.designer.cs
+++ SistemaReparaciónDePC/Cliente_Index.aspx.designer.cs
@@ -13,5 +13,50 @@ namespace SistemaReparaciónDePC
 
     public partial class Cliente_Index
     {
+
+        /// <summary>
+        /// Control gvCliente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gvCliente;
+
+        /// <summary>
+        /// Control lb1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl lb1;
+
+        /// <summary>
+        /// Control txtbuscar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText txtbuscar;
+
+        /// <summary>
+        /// Control btnbuscar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlButton btnbuscar;
+
+        /// <summary>
+        /// Control btnnuevo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlButton btnnuevo;
     }
 }
