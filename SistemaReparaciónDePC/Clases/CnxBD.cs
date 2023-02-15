1f19c6bca7442117ba818349107a6d8ddfa0ab93
juancenteno10 <juancsabillon06@gmail.com>
2023-02-15 10:40:45 -0600

    Conexion base de datos

--- SistemaReparaciónDePC/Clases/CnxBD.cs
+++ SistemaReparaciónDePC/Clases/CnxBD.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,27 @@ namespace SistemaReparaciónDePC.Clases
 {
     public class CnxBD
     {
+        SqlConnection cnx = new SqlConnection();
+        string con = "";
+        public void coneccion ()
+        {
+           
+            con = @"Data Source =DESKTOP-TSV52PA\SQLEXPRESS; Initial catalog=BD_Reparaciones; user = JuanCenteno; password=juancenteno20 ";
+            cnx.ConnectionString = con;
+        }
+        public SqlConnection Obtenercnx()
+        {
+            coneccion();
+            return cnx;
+        }
+
+
+
+
+
+
+
+
 
     }
 }
