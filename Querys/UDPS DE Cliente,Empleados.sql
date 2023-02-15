0ddcfbd827be9baa9933891e5e209fd8a6288954
Angie Campos <angie.camposyc03@gmail.com>
2023-02-15 13:42:28 -0600

    Cambios a Clientes

--- Querys/UDPS DE Cliente,Empleados.sql
+++ Querys/UDPS DE Cliente,Empleados.sql
@@ -13,13 +13,12 @@ BEGIN
 		  INNER JOIN [dbo].[tbl_EstadoCivil] T2 ON T1.Cliente_EstadoCivilId = T2.EstadoCivil_ID
 		  INNER JOIN [dbo].[tbl_Ciudades] T3 ON T1.Cliente_CiudadId = T3.Ciudad_Id 
 		  where Cliente_Estado=1
-
-
 END 
 
 EXECUTE UDP_MostrarClientes 
 
-cREATE OR ALTER pROCEDURE UDP_BuscarClientes
+GO
+CREATE OR ALTER pROCEDURE UDP_BuscarClientes
    @buscador  nvarchar(max)
 as 
 begin 
@@ -39,7 +38,26 @@ begin
 		  '%'+@buscador+'%' or Cliente_EstadoCivilId = '%'+@buscador+'%' or T3.Ciudad_Descripcion = '%'+@buscador+'%' 
 end 
 
+EXEC UDP_BuscarClientes 'c'
+GO
 
+-- udp para obtener datos
+CREATE OR ALTER PROCEDURE UDP_ObtenerDatos_Cliente
+		@ID  INT
+AS
+BEGIN
+		SELECT	[Cliente_Id], [Cliente_Nombre] + [Cliente_Apellido] AS 'Cliente', [Cliente_Identidad],
+				T2.EstadoCivil_Descripcion, [Cliente_Genero],[Cliente_Telefono] ,T3.Ciudad_Descripcion
+		FROM	[dbo].[tbl_Cliente] T1				INNER JOIN [dbo].[tbl_EstadoCivil] T2
+		ON		T1.Cliente_EstadoCivilId = T2.EstadoCivil_ID	INNER JOIN [dbo].[tbl_Ciudades] T3
+		ON		T1.Cliente_CiudadId = T3.Ciudad_Id
+		WHERE	[Cliente_Id] = @ID
+END
+
+EXEC UDP_ObtenerDatos_Cliente 2
+GO
+
+--
 Create or Alter Procedure UDP_EditarClientes
    @id                          int ,
    @Nombre                      nvarchar(250),
@@ -61,7 +79,7 @@ begin
 	 where [Cliente_Id]=@id
 end 
 
-
+go
 Create or Alter Procedure UDP_InsertCliente
    @Nombre                      nvarchar(250),
    @Apellido                    nvarchar(250),
@@ -77,18 +95,19 @@ begin
    Insert into [dbo].[tbl_Cliente]
    values (@Nombre,@Apellido,@identidad,@EstadoCivil,@genero,@telefono,@ciudad, @usuariocreacion,GEtdate(),null,null,1)
 end 
-
+go
 
 Create or Alter Procedure UDP_EliminarCliente     
-    @idAeliminar  int 
+    @idAeliminar  int ,
+	@UsuarioModi  int
 as
 begin 
-   update [dbo].[tbl_Empleados]
-   set [Empleado_Estado]=0
-   Where [Empleado_Id]=@idAeliminar
+   update	[dbo].[tbl_Cliente]
+   set		[Cliente_Estado]=0, [Cliente_UsuarioModificacionId] = @UsuarioModi, [Cliente_FechaModificacion] = GETDATE()
+   Where	[Cliente_Id]=@idAeliminar
 
 end 
-
+go
 
 
 
@@ -110,7 +129,7 @@ SELECT       Empleado_Id,
    			 ON T1.Empleado_CiudadId= T3.Ciudad_Id
 			 Where [Empleado_Estado]=1
 end 
-
+go
 
 cREATE OR aLTER pROCEDURE UDP_BuscarEmpleados
       @buscador    nvarchar(max)
@@ -134,7 +153,7 @@ begin
 			 or Empleado_Puesto like '%'+@buscador+'%' or T2.EstadoCivil_Descripcion = '%'+@buscador+'%'
 			 or T3.Ciudad_Descripcion like '%'+@buscador+'%'
 end
-
+go
 Create or alter Procedure UDP_EditarEmpleados
       @id           int, 
 	  @Nombre       nvarchar(250),
@@ -155,7 +174,7 @@ begin
       [Empleado_Puesto]=@puesto,[Empleado_UsuarioModificacionId]=@usuariomodifica,[Empleado_FechaModificacion]=GETDATE()
       WHERE [Empleado_Id]=@id 
 end 
-
+go
 Create or Alter Procedure UDP_EliminarEmpleado
 
       @idaEliminar   int 
@@ -166,7 +185,7 @@ begin
 	 where [Empleado_Estado] = @idaEliminar 
 
 end 
-
+go
 ----------------UDP'S  Producto--------------- 
 Create or alter procedure UDP_MostrarProducto
 as
@@ -182,7 +201,7 @@ begin
 		   Accion
    from  [dbo].[tbl_Producto]
 end 
-
+go
 Create or Alter procedure UDP_BuscarProducto
     @buscador     nvarchar(250)
 as 
@@ -200,7 +219,7 @@ begin
    Where pro_ID like '%'+@buscador+'%' or pro_Descripción like'%'+@buscador+'%' or pro_FechaIngreso like '%'+@buscador+'%' 
 end 
 
-
+go
 Create Or Alter Procedure UDP_EditarProducto
        @id                int, 
 	   @descripcion       nvarchar(250),
@@ -210,7 +229,9 @@ as
 
 begin 
      Update [dbo].[tbl_Producto]
-	 Set pro_Descripción = @descripcion, pro_FechaIngreso=@Fecha,UsuarioModificacion=@Usumodificacion
+	 Set pro_Descripción = @descripcion, pro_FechaIngreso=@Fecha,UsuarioModificacion=@Usumodificacion, 
+	     [FechaModificacion] = GETDATE()
+	WHERE [pro_ID] = @id
 end 
 
 Create or Alter Procedure UDP_InsertarProducto
