cc249bb4c73dadeb291ebc724746d50c9bc612d0
juancenteno10 <juancsabillon06@gmail.com>
2023-02-16 02:21:05 -0600

    insert empleados

---
+++ SistemaReparaciónDePC/InsertarEmpleados.aspx.cs
@@ -0,0 +1,105 @@
+﻿using SistemaReparaciónDePC.Clases;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SistemaReparaciónDePC
+{
+    public partial class InsertarEmpleados : System.Web.UI.Page
+    {
+        Cliente cli = new Cliente();
+        Utilitario util = new Utilitario();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                cli.DdlEstado(ddlestadocivil);
+                cli.ddldepto(ddlDepartamento);
+            }
+        }
+
+        protected void ddlDepartamento_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cli.ddlmuni(ddlMuni, ddlDepartamento.SelectedValue);
+        }
+
+        protected void btnGuardar_Click(object sender, EventArgs e)
+        {
+            string sexo = "";
+
+
+            if (txtnombre.Value == "")
+            {
+                lblNombre.Visible = true;
+            }
+            if (txtapellido.Value == "")
+            {
+                lblapellido.Visible = true;
+            }
+            if (txtIdentidad.Value == "")
+            {
+                lblidentidad.Visible = true;
+            }
+            if (txttelefono.Value == "")
+            {
+                lbltelefono.Visible = true;
+            }
+            if (ddlMuni.SelectedValue == "")
+            {
+                lblLugar.Visible = true;
+            }
+            if (ddlestadocivil.SelectedValue == "")
+            {
+                lblEstado.Visible = true;
+            }
+            if (txtpuesto.Value == "")
+            {
+                lblPuesto.Visible = true;
+            }
+
+            if (radiof.Checked == true)
+            {
+                sexo = "F";
+            }
+            if (radiom.Checked == true)
+            {
+                sexo = "M";
+            }
+
+            if (txtnombre.Value != "" && txtapellido.Value != "" && txtIdentidad.Value != "" && txttelefono.Value != ""
+             && ddlMuni.SelectedValue != "")
+            {
+                if (radiof.Checked == true || radiom.Checked == true)
+                {
+                    DataSet ds = new DataSet();
+                    string sql = "UDP_InsertarEmpleados '" + txtnombre.Value + "','" + txtapellido.Value + "'" +
+                    ",'" + txtIdentidad.Value + "','" + ddlestadocivil.SelectedValue + "','" + sexo + "','" + txttelefono.Value + "','" + ddlMuni.SelectedValue + "','" + txtpuesto.Value + "' ,'" + 1 + "' ";
+                    ds = util.ObtenerDatos(sql, "TUsu");
+                    //Limpiar();
+                }
+            }
+        }
+        public void Limpiar()
+        {
+            txtnombre.Value = "";
+            txtapellido.Value = "";
+            txtIdentidad.Value = "";
+            txttelefono.Value = "";
+            ddlestadocivil.SelectedValue = "0";
+            ddlDepartamento.SelectedValue = "0";
+            ddlMuni.SelectedValue = "";
+            radiof.Checked = false;
+            radiom.Checked = false;
+            txtpuesto.Value = "";
+        }
+        protected void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+    }
+
+}
---
+++ SistemaReparaciónDePC/InsertarEmpleados.aspx.designer.cs
@@ -0,0 +1,197 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace SistemaReparaciónDePC
+{
+
+
+    public partial class InsertarEmpleados
+    {
+
+        /// <summary>
+        /// Control lblNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblNombre;
+
+        /// <summary>
+        /// Control txtnombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText txtnombre;
+
+        /// <summary>
+        /// Control lblapellido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblapellido;
+
+        /// <summary>
+        /// Control txtapellido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText txtapellido;
+
+        /// <summary>
+        /// Control lblidentidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblidentidad;
+
+        /// <summary>
+        /// Control txtIdentidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText txtIdentidad;
+
+        /// <summary>
+        /// Control lblEstado.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblEstado;
+
+        /// <summary>
+        /// Control ddlestadocivil.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlestadocivil;
+
+        /// <summary>
+        /// Control radiof.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputRadioButton radiof;
+
+        /// <summary>
+        /// Control radiom.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputRadioButton radiom;
+
+        /// <summary>
+        /// Control lbltelefono.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbltelefono;
+
+        /// <summary>
+        /// Control txttelefono.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText txttelefono;
+
+        /// <summary>
+        /// Control lblLugar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblLugar;
+
+        /// <summary>
+        /// Control ddlDepartamento.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlDepartamento;
+
+        /// <summary>
+        /// Control ddlMuni.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlMuni;
+
+        /// <summary>
+        /// Control lblPuesto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblPuesto;
+
+        /// <summary>
+        /// Control txtpuesto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText txtpuesto;
+
+        /// <summary>
+        /// Control Advertencia.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Advertencia;
+
+        /// <summary>
+        /// Control btnGuardar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnGuardar;
+
+        /// <summary>
+        /// Control btnLimpiar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnLimpiar;
+    }
+}
