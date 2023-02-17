c840723e1e099d1da23d7ce8313526909c6162f4
Angie Campos <angie.camposyc03@gmail.com>
2023-02-17 11:48:28 -0600

    Cerrar Sesion Boton Programacion

--- SistemaReparaciónDePC/PaginaMaster.Master.designer.cs
+++ SistemaReparaciónDePC/PaginaMaster.Master.designer.cs
@@ -15,6 +15,15 @@ namespace SistemaReparaciónDePC
     {
 
         /// <summary>
+        /// Control btnCerrarSesion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnCerrarSesion;
+
+        /// <summary>
         /// Control ScriptManager1.
         /// </summary>
         /// <remarks>
