4d7322148cb4a8efaa41b49eb150be2e270c81db
Angie Campos <angie.camposyc03@gmail.com>
2023-02-17 11:17:56 -0600

    LogIn

--- SistemaReparaciónDePC/LogIn.aspx.designer.cs
+++ SistemaReparaciónDePC/LogIn.aspx.designer.cs
@@ -24,6 +24,15 @@ namespace SistemaReparaciónDePC
         protected global::System.Web.UI.WebControls.Image img;
 
         /// <summary>
+        /// Control lblDatos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblDatos;
+
+        /// <summary>
         /// Control lblUsu.
         /// </summary>
         /// <remarks>
